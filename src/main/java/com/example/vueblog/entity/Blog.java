c3ce0480c6fa32ff48370ac2a8c459c829193d4b
zhonghanlu <1420865757@qq.com>
2023-04-22 23:50:32 +0800

    Improvement

--- src/main/java/com/example/vueblog/entity/Blog.java
+++ src/main/java/com/example/vueblog/entity/Blog.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.time.LocalDateTime;
 import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -36,6 +38,7 @@ public class Blog implements Serializable {
 
     private String content;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime created;
 
     private Integer status;
